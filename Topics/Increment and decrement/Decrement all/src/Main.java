0d4473c69bf3412f04b2d5c65763d74a0c783e17
GitDerentis <53261043+GitDerentis@users.noreply.github.com>
2021-10-07 13:11:48 +0200

    Add files via upload

---
+++ Topics/Increment and decrement/Decrement all/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // put your code here
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println(--a + " " + --b + " " + --c + " " + --y);
+    }
+}
